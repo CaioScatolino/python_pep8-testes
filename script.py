ad9c67c3cff96b4024a6839192ee69bf069ce6d3
Caio Scatolino <caio.scatolino@outlook.com>
2026-04-02 14:41:32 -0300

    Iniciando projeto, inserindo itens na lista, etc

---
+++ script.py
@@ -0,0 +1,30 @@
+class List:
+    def __init__(self, name):
+        self._filename = f"{name}.txt"
+
+    def get_list(self):
+        list = []
+        with open(self._filename, "r") as arquivo:
+            for linha in arquivo:
+                list.append(linha.strip())
+        return list
+
+    def add_item(self, item):
+        with open(self._filename, "a") as arquivo:
+            if item != "":
+                arquivo.write(f"\n{item}")
+                return True
+            else:
+                return False
+
+
+list = List("lista")
+for item in list.get_list():
+    print(f"-- {item}")
+
+new_item = input("Digite o item que deseja adicionar: ")
+added = list.add_item(new_item)
+if added == True:
+    print("Item adicionado com sucesso!")
+else:
+    print("Item inválido!")
